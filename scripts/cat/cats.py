cb1a964970f6f809a7ae06ad9600113aa29e7935
Rusty <rusty3925@gmail.com>
2024-11-06 06:35:58 -0800

    generate relationships with instructor if cat knew them in life

--- scripts/cat/cats.py
+++ scripts/cat/cats.py
@@ -2664,7 +2664,7 @@ class Cat:
                 trust = 0
                 if game.settings["random relation"]:
                     if game.clan:
-                        if the_cat == game.clan.instructor:
+                        if the_cat == game.clan.instructor and game.clan.instructor.dead_for >= self.moons:
                             pass
                         elif randint(1, 20) == 1 and romantic_love < 1:
                             dislike = randint(10, 25)
