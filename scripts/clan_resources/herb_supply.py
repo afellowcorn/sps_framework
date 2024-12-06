9a190dfece3e3f8eb66922f2574a80e82f9aa104
scribblecrumb <abspence2199@gmail.com>
2024-12-05 22:31:36 -0600

    basics of herb class
    functions to:
    - check supply
    - get total supply of indv and all herbs
    - add herbs
    - remove herbs

---
+++ scripts/clan_resources/herb_supply.py
@@ -0,0 +1,104 @@
+import ujson
+
+
+class HerbSupply:
+    """Handles managing the Clan's herb supply."""
+
+    def __init__(self, herb_supply: dict = None):
+        """
+        Initialize the class
+        """
+        # a dict of current stored herbs - herbs collected this moon
+        self.herb_supply: dict = herb_supply if herb_supply else {}
+
+        # a dict of herbs collected this moon
+        self.herbs_collected: dict = {}
+
+        # total of all herbs: both stored and collected this moon
+        self.total_herb_count: int = self.get_supply_total()
+
+    """
+    this needs to:
+    - track expiration of herbs
+        - do so via a dict of lists. key is the herb, list is a buffer of herb amt collected with each item being a moon of time
+    - handle removal of expired herbs
+    - return helpful info
+        - if supply is enough for clan
+    """
+
+    def check_supply(self, herb: str) -> int:
+        """
+        checks for given herb and returns current int of supply
+        """
+        full_count = 0
+        for stock in self.herb_supply[herb]:
+            full_count += stock
+
+        return full_count
+
+    def get_supply_total(self) -> int:
+        """
+        return total int of all herb inventory
+        """
+        total = 0
+        for herb in self.herb_supply:
+            for stock in self.herb_supply[herb]:
+                total += stock
+        for herb in self.herbs_collected:
+            total += herb
+
+        return total
+
+    def get_single_herb_total(self, herb: str) -> int:
+        """
+        returns int total stock of given herb
+        """
+        total = 0
+        for stock in self.herb_supply[herb]:
+            total += stock
+
+        for amt in self.herbs_collected[herb]:
+            total += amt
+
+        return total
+
+    def add_herb(self, herb: str, num_collected: int):
+        """
+        adds herb given to count for that moon
+        """
+
+        if self.herbs_collected.get(herb, []):
+            self.herbs_collected[herb] += num_collected
+        else:
+            self.herbs_collected[herb] = num_collected
+
+    def remove_herb(self, herb: str, num_removed: int):
+        """
+        removes herb given from count for that moon, then from supply if necessary
+        """
+        surplus = 0
+
+        if self.herbs_collected.get(herb, []):
+            self.herbs_collected[herb] += num_removed
+            if self.herbs_collected[herb] < 0:
+                num_removed = abs(self.herbs_collected[herb])
+                self.herbs_collected[herb] = 0
+            else:
+                return
+
+        if num_removed:
+            self._get_from_supply(herb, num_removed)
+
+    def _get_from_supply(self, herb: str, needed_num: int):
+        """
+        removes needed_num of given herb from supply until needed_num is met or supply is empty
+        """
+        while needed_num > 0 and self.herb_supply[herb]:
+            self.herb_supply[herb][-1] -= needed_num
+            if self.herb_supply[herb][-1] > 0:
+                needed_num = abs(self.herb_supply[herb][-1])
+                self.herb_supply[herb].pop(-1)
+
+
+with open("resources/dicts/herbs.json", "r", encoding="utf-8") as read_file:
+    HERBS = ujson.loads(read_file.read())
