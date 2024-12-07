d372de9758c3546fedb7589ae2a95a230b26e233
scribblecrumb <abspence2199@gmail.com>
2024-12-06 21:49:17 -0600

    small adjustments

--- scripts/clan_resources/herb.py
+++ scripts/clan_resources/herb.py
@@ -9,25 +9,25 @@ class Herb:
             biome
             ):
         self.name: str = herb_name
-        self.herb_dict: dict = HERBS.get(self.name, {})
+        self._herb_dict: dict = HERBS.get(self.name, {})
 
-        self.clan_biome: str = season.casefold()
-        self.game_season: str = biome.casefold()
+        self._clan_biome: str = season.casefold()
+        self._game_season: str = biome.casefold()
 
-        self.display_dict = self.herb_dict.get("display", {})
-        self.singular_display = self.display_dict.get("singular", self.name)
-        self.plural_display = self.display_dict.get("plural", self.name)
+        self._display_dict = self._herb_dict.get("display", {})
+        self.singular_display = self._display_dict.get("singular", self.name)
+        self.plural_display = self._display_dict.get("plural", self.name)
 
-        self.expiration: int = self.herb_dict.get("expiration", 1)
+        self.expiration: int = self._herb_dict.get("expiration", 1)
 
     @property
     def rarity(self):
         """
         rarity of the herb within clan's current biome and season
         """
-        rarity_dict = self.herb_dict.get("rarity", {})
+        rarity_dict = self._herb_dict.get("rarity", {})
 
-        return rarity_dict.get(self.clan_biome, {}).get(self.game_season, 0)
+        return rarity_dict.get(self._clan_biome, {}).get(self._game_season, 0)
 
 
 with open("resources/dicts/herbs.json", "r", encoding="utf-8") as read_file:
