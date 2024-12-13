8287eba45459be48580a7f682d93889e4fca6767
Lara <63805048+larkgz@users.noreply.github.com>
2024-12-12 22:33:29 -0500

    test: add jsonschema tests for patrols

--- tests/test_json_schemas.py
+++ tests/test_json_schemas.py
@@ -20,13 +20,16 @@ RESOURCES_DIR = ROOT_DIR / "resources" / "dicts"
 
 COMMON_SCHEMA = json.loads((SCHEMA_DIR / "common.schema.json").read_text())
 THOUGHT_SCHEMA = json.loads((SCHEMA_DIR / "thought.schema.json").read_text())
+PATROL_SCHEMA = json.loads((SCHEMA_DIR / "patrol.schema.json").read_text())
 
 THOUGHT_DIR = RESOURCES_DIR / "thoughts"
+PATROL_DIR = RESOURCES_DIR / "patrols"
 
 registry = Registry().with_resources(
     [
         ("common.schema.json", DRAFT7.create_resource(COMMON_SCHEMA)),
-        ("thought.schema.json", DRAFT7.create_resource(THOUGHT_SCHEMA))
+        ("thought.schema.json", DRAFT7.create_resource(THOUGHT_SCHEMA)),
+        ("patrol.schema.json", DRAFT7.create_resource(PATROL_SCHEMA))
     ]
 )
 
@@ -36,11 +39,29 @@ def all_thought_files():
     """
     yield from THOUGHT_DIR.glob("**/*.json")
 
+def all_patrol_files():
+    """
+    Iterator for Paths for all patrol files
+    """
+    EXCLUSIONS = [
+        PATROL_DIR / "explicit_patrol_art.json"
+    ]
+    for filename in PATROL_DIR.glob("**/*.json"):
+        if filename not in EXCLUSIONS:
+            yield filename
+
 def test_thoughts_schema():
     """Test that all thought JSONs are correct according to the JSON schema"""
     for thought_file in all_thought_files():
         data = json.loads(thought_file.read_text())
         jsonschema.validate(data, THOUGHT_SCHEMA, cls=jsonschema.Draft7Validator,
+                            registry=registry)
+
+def test_patrols_schema():
+    """Test that all patrol JSONs are correct according to the JSON schema"""
+    for patrol_file in all_patrol_files():
+        data = json.loads(patrol_file.read_text())
+        jsonschema.validate(data, PATROL_SCHEMA, cls=jsonschema.Draft7Validator,
                             registry=registry)
 
 class TestJsonSchemas(unittest.TestCase):
@@ -52,5 +73,11 @@ class TestJsonSchemas(unittest.TestCase):
            thought JSONs are correct according to the JSON schema."""
         test_thoughts_schema()
 
+    def test_patrols_schema(self):
+        """Unittest for local use to test that all
+           patrol JSONs are correct according to the JSON schema."""
+        test_patrols_schema()
+
 if __name__ == "__main__":
     test_thoughts_schema()
+    test_patrols_schema()
