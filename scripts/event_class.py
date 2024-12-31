3104bb6c4c6ab18ba1540aada7142978712b99a2
scribblecrumb <abspence2199@gmail.com>
2024-12-31 15:08:51 -0600

    fix save crash

--- scripts/event_class.py
+++ scripts/event_class.py
@@ -43,20 +43,26 @@ class Single_Event:
         if self.cat_dict and self.cats_involved == []:
             self.cats_involved = [cat.ID for cat in self.cat_dict.values()]
 
+
+
     def to_dict(self):
         """
         Convert Single_Event to dictionary.
         """
+        cat_dict = self.cat_dict.copy() if self.cat_dict else {}
+        if self.cat_dict:
+            for abbr, kitty in self.cat_dict.items():
+                cat_dict[abbr] = kitty.ID
 
         return {
             "text": self.text,
             "types": self.types,
             "cats_involved": self.cats_involved,
-            "cat_dict": self.cat_dict,
+            "cat_dict": cat_dict,
         }
 
     @staticmethod
-    def from_dict(dict):
+    def from_dict(dict, Cat):
         """
         Return new Single_Event based on dict.
 
@@ -65,9 +71,15 @@ class Single_Event:
 
         if "text" not in dict:
             return None
+
+        cat_dict = dict.get("cat_dict", None)
+        if cat_dict:
+            for abbr, kitty in cat_dict.copy().items():
+                cat_dict[abbr] = Cat.fetch_cat(kitty)
+
         return Single_Event(
             text=dict["text"],
             types=dict.get("types", None),
             cats_involved=dict.get("cats_involved", None),
-            cat_dict=dict.get("cat_dict", None),
+            cat_dict=cat_dict,
         )
