331ff69e9e22dd8e0a689ce6d026cdd6a68f2ded
Rusty <rusty3925@gmail.com>
2025-01-18 01:04:00 +0000

    make sure command succeeds before running next one

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "$0")" # script location
-poetry install --no-root
+# cd into script location
+cd "$(dirname "$0")" && \
+poetry install --no-root && \
 poetry run python main.py
+
