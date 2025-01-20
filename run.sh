6f4b2c872c1be8da04c14bd6d9f5c2f51cac1a53
Rusty <rusty3925@gmail.com>
2025-01-19 17:40:19 -0800

    exit on failure

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-# cd into script location
-cd "$(dirname "$0")" && \
-poetry install --no-root && \
-poetry run python main.py
+set -e # exit on failure
 
+# cd into script location
+cd "$(dirname "$0")"
+poetry install --no-root
+poetry run python main.py
